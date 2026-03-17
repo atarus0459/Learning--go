9c0ff709501bb4d3673560dc1129390966104213
atarus <biiallankipkoech@gmail.com>
2026-03-17 17:00:52 +0300

    Add functions with parameters

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import "fmt"
 
-func main() {
-	score := 75
-
-	if score >= 80 {
-		fmt.Println("Grade A")
-	} else if score >= 70 {
-		fmt.Println("Grade A-")
-	} else if score >= 60 {
-		fmt.Println("Grade B+")
-	}else {
-		fmt.Println("Grade C")
-	}
+func greet() {
+	fmt.Println("Good evening Allan")
 }
-	
+
+func main() {
+	greet()
+}
