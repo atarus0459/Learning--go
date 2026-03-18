7e68fdac1886d032649cc39a4287184df6554b21
atarus <biiallankipkoech@gmail.com>
2026-03-18 13:51:14 +0300

    Add printmessage function

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import "fmt"
 
-func multiply(a int, b int) (result int) {
-	result = a * b
-	return result
+func printMessage() {
+	fmt.Println("Hello, World!")
 }
 
 func main() {
-	result := multiply(2, 4)
-	fmt.Println(result)
+	printMessage()
 }
